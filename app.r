397e11e9b68133eb2ec6567ec6de19296a618832
fednem <federico.nemmi@gmail.com>
2018-03-01 12:06:24 +0100

    first commit with working app

--- app.r
+++ app.r
@@ -17,11 +17,9 @@ ui <- fluidPage(mainPanel(
                         sliderInput("year_range", "Select the time range to plot",
                                     min = min_year,
                                     max = max_year, , value = c(min_year, max_year), step = 1, sep=""),
-                        selectInput("index", "Index to plot", choices = indexes),
-                        selectInput("grouping", "Aggregation level", choices = c("Country",
-                                                                                 "Continent",
-                                                                                 "Region"))),
-                        
+                        selectInput("index", "Index to plot", choices = indexes)),
+                        selectInput("country", "Country to plot", 
+                                    choices = countries, selectize = TRUE, multiple = TRUE, selected = "Italy"),
                       plotOutput("timeseries")
                     )
                   )
@@ -31,34 +29,14 @@ ui <- fluidPage(mainPanel(
                       
   
 server <- function(input,output){
-  output$timeseries <- renderPlot({ if(input$grouping == "Country") {
-                                                                    df <- data_filtered_indicators_country_partitions %>%
-                                                                          filter(Year > input$year_range[1],  
-                                                                                 Year < input$year_range[2],
-                                                                                 IndicatorName == input$index)
-                                                                    ggplot(data = df, aes(x = Year, y = Value, group = CountryCode,
-                                                                    color = CountryCode)) + 
-                                                                    geom_line()} else if (input$grouping == "Continent") {
-                                                                      df <- data_filtered_indicators_country_partitions %>%
-                                                                        filter(Year > input$year_range[1],  
-                                                                               Year < input$year_range[2],
-                                                                               IndicatorName == input$index) %>%
-                                                                        group_by(Continent, Year) %>%
-                                                                        summarise(average = mean(Value))
-                                                                      ggplot(data = df, aes(x = Year, y = average, 
-                                                                                            group = Continent, color = Continent)) + 
-                                                                        geom_line()} else if (input$grouping == "Region") {
-                                                                          df <- data_filtered_indicators_country_partitions %>%
-                                                                            filter(Year > input$year_range[1],  
-                                                                                   Year < input$year_range[2],
-                                                                                   IndicatorName == input$index) %>%
-                                                                            group_by(Region, Year) %>%
-                                                                            summarise(average = mean(Value))
-                                                                          ggplot(data = df, aes(x = Year, y = average, 
-                                                                                                group = Region, color = Region)) + 
-                                                                            geom_line()}
-                                        
-                                      })}
+  
+  output$timeseries <- renderPlot({df <- data_filtered_indicators_country_partitions %>%
+    filter(Year > input$year_range[1],  
+           Year < input$year_range[2],
+           IndicatorName == input$index,
+           CountryName %in% input$country)
+        ggplot(data = df, aes(x = Year, y = Value, group = CountryName, color = CountryName)) + 
+                                                                            geom_line()})}
   
 
 
