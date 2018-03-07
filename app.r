218f5cdeeb437cf68e2cd29cad5a87789d29a967
fednem <federico.nemmi@gmail.com>
2018-03-07 17:09:45 +0100

    added updating for the scatterplot

--- app.r
+++ app.r
@@ -23,36 +23,37 @@ data_by_region <- data_filtered_indicators_country_partitions %>%
 
 
 ui <- fluidPage(mainPanel(
-                  tabsetPanel(
-                    tabPanel("TimeSeries", 
-                      inputPanel(
-                        sliderInput("year_range", "Select the time range to plot",
-                                    min = min_year,
-                                    max = max_year, , value = c(min_year, max_year), step = 1, sep=""),
-                        selectInput("index", "Index to plot", choices = indexes),
-                        selectInput("country", "Country to plot", 
-                                    choices = countries, selectize = TRUE, multiple = TRUE, selected = "Italy")),
-                      plotOutput("timeseries")
-                    ),
-                    tabPanel("Relationship between indexes",
-                      inputPanel(
-                        selectInput("x_axis", "Select the variable on the x axis",
-                                    choices = indexes),
-                        selectInput("y_axis", "Select the variable on the y axis",
-                                    choices = indexes), 
-                        selectInput("year_relation", "Select the year you want the data for",
-                                    choices = c(min_year:max_year),
-                                    selected = 1984),
-                        radioButtons("color_by", "Select colour code",
-                                     choices = c("Continent" = "Continent", "Macro Region" = "Region"))),
-                        plotOutput("relation")
-                    )
-                  )
+  tabsetPanel(
+    tabPanel("TimeSeries", 
+             inputPanel(
+               sliderInput("year_range", "Select the time range to plot",
+                           min = min_year,
+                           max = max_year, , value = c(min_year, max_year), step = 1, sep=""),
+               selectInput("index", "Index to plot", choices = indexes),
+               selectInput("country", "Country to plot", 
+                           choices = countries, selectize = TRUE, multiple = TRUE, selected = "Italy")),
+             plotOutput("timeseries")
+    ),
+    tabPanel("Relationship between indexes",
+             inputPanel(
+               selectInput("x_axis", "Select the variable on the x axis",
+                           choices = indexes),
+               selectInput("y_axis", "Select the variable on the y axis",
+                           choices = indexes), 
+               selectInput("year_relation", "Select the year you want the data for",
+                           choices = c(min_year:max_year),
+                           selected = 1984),
+               radioButtons("color_by", "Select colour code",
+                            choices = c("Continent" = "Continent", "Macro Region" = "Region")),
+               actionButton("update_relation","Update plot")),
+             plotOutput("relation")
+    )
   )
 )
+)
 
-                      
-  
+
+
 server <- function(input,output){
   
   output$timeseries <- renderPlot({df <- data_filtered_indicators_country_partitions %>%
@@ -60,25 +61,24 @@ server <- function(input,output){
            Year < input$year_range[2],
            IndicatorName == input$index,
            CountryName %in% input$country)
-    ggplot(data = df, aes(x = Year, y = Value, group = CountryName, color = CountryName)) + 
-            geom_line()})
-  output$relation <- renderPlot({df <- data_filtered_indicators_country_partitions %>%
+  ggplot(data = df, aes(x = Year, y = Value, group = CountryName, color = CountryName)) + 
+    geom_line()})
+  df_relation <- eventReactive(input$update_relation, {df <- data_filtered_indicators_country_partitions %>%
     filter(Year == input$year_relation,
            IndicatorName %in% c(input$x_axis, input$y_axis)) %>%
     split(., .$IndicatorName) %>%
     map(~spread(., IndicatorName, Value)) %>%
     reduce(left_join, by = "CountryCode") %>%
     select(-ends_with("y"))
-    colnames(df)[1:6] <- colnames(df)[1:6] %>% 
-      str_split(., pattern = ".x") %>%
-      map_chr(~`[`(.,1))
-  
-  ggplot(data = df, aes_string(x = as.name(input$x_axis), y = as.name(input$y_axis), color = input$color_by)) + 
-          geom_point()})
+  colnames(df)[1:6] <- colnames(df)[1:6] %>% 
+    str_split(., pattern = ".x") %>%
+    map_chr(~`[`(.,1))
+  df})
+  output$relation <- renderPlot({
+  ggplot(data = df_relation(), aes_string(x = isolate(as.name(input$x_axis)), y = isolate(as.name(input$y_axis)), color = isolate(input$color_by))) + 
+    geom_point()})
 }
-  
+
 
 
 shinyApp(ui = ui, server = server)
-
-
